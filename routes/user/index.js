dbdea329be7fae1679a9d6e4daf5ec613841dfe3
shyam22693 <shyamkumar22693@gmail.com>
2018-10-25 18:32:00 +0530

    payment airpay

--- routes/user/index.js
+++ routes/user/index.js
@@ -1,10 +1,18 @@
 var express = require('express');
 var router = express.Router();
+
 var subscription = require('../../lib/models/subscription');
 let subscriptionController = require('../../lib/src/js/scriptController/subscriptionController');
 let localityController = require('../../lib/src/js/scriptController/localityController');
 let apartmentController = require('../../lib/src/js/scriptController/apartmentController');
 let userController = require('../../lib/src/js/scriptController/userController');
+
+
+var mid = '24324';
+var username = '5168160';
+var password = 'NBTh6GXx';
+var secret = 'KVnqNAjbh8wKE6wM';
+var now = new Date();
 
 var locality = require('../../lib/models/locality')
 
@@ -17,6 +25,24 @@ router.post('/sendotp', function(req, res) {
   	  console.log('req.body',signupotp)
   	  console.log('req.body',phone)
   	  // res.send(signupotp)
+});
+
+//payment integration
+
+router.post('/sendtoairpay', function(req, res, next) {
+	console.log('req.body',req.body);
+	var md5 = require('md5');
+	var sha256 = require('sha256');
+	var dateformat = require('dateformat');
+    alldata   = req.body.buyerAddress+req.body.amount+req.body.orderid;
+    udata = username+':|:'+password;
+    privatekey = sha256(secret+'@'+udata);
+    aldata = alldata+dateformat(now,'yyyy-mm-dd');
+	checksum = md5(aldata+privatekey);
+    fdata = req.body;
+    let paymentObj = { mid : mid,data: fdata,privatekey : privatekey,checksum:checksum}
+    console.log('paymentObj paymentObj',paymentObj)
+    res.send(paymentObj);
 });
 
 // subcription
