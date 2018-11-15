48cc85bfbcb0878787cdc3f8b37f92cf47b6f272
Puneet Naik <puneet.naik@ritsworld.com>
2018-11-15 17:36:07 +0530

    s

--- routes/user/index.js
+++ routes/user/index.js
@@ -22,16 +22,22 @@ var locality = require('../../lib/models/locality')
 
 //send otp sign up functionalities
 router.post('/sendotp', function(req, res) {
-	  let signupotp = Math.floor(1000 + Math.random() * 9000);
-      let phone = [];
-      phone.push(Number(req.body.mobile));
-  	  console.log('req.body',req.body)
-  	  console.log('req.body',signupotp)
-  	  console.log('req.body',phone)
-  	  // res.send(signupotp)
+    let signupotp = Math.floor(1000 + Math.random() * 9000);
+    let phone = [];
+    phone.push(Number(req.body.mobile));
+    console.log('req.body',req.body)
+    console.log('req.body',signupotp)
+    console.log('req.body',phone)
+    // res.send(signupotp)
 });
 
 //payment integration
+
+router.post('/thankyou', function(req, res, next) {
+   console.log(req.body,'req') 
+   console.log(res,'req')
+   res.redirect(`/`) 
+}
 
 router.post('/sendtoairpay', function(req, res, next) {
 	console.log('req.body',req.body);
