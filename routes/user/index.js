8e9f753003bdcf0995b599ddd67235d81cfde74a
Puneet Naik <puneet.naik@ritsworld.com>
2018-12-04 19:25:49 +0530

    changes in pages

--- routes/user/index.js
+++ routes/user/index.js
@@ -27,18 +27,18 @@ router.post('/sendotp', function(req, res) {
 });
 
 //payment integration
-
 router.post('/thankyou', function(req, res, next) {
-   console.log(req.body,'req') 
-   console.log(res,'req')
-   if(req.body.MESSAGE == 'Success'){
+    if(req.body.MESSAGE == 'Success'){
         res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
         res.header('Expires', '-1');
         res.header('Pragma', 'no-cache');
         res.redirect(`/thankyou`)
-   }else{
+    }else{
         res.redirect(`/declined`)
-   }
+    }
+})
+router.post('/declined', function(req, res, next) {
+    res.redirect('/declined')
 })
 
 router.post('/sendtoairpay', function(req, res, next) {
